ac73bb3549e84600cc4f6afb391bb1e5ef83a111
Stanley Varnhagen <stanley.varnhagen@ualberta.ca>
2018-11-16 19:24:23 -0500

    initial swift commit

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Stanley Varnhagen on 2018-11-14.
+//  Copyright © 2018 Stanley Varnhagen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
